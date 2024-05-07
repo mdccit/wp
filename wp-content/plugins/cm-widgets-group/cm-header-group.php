6a0fdc7581d0108bab5efbb24be24d7b2c80825f
Ashan R <ashan.rajapaksha@qualitapps.com>
2024-05-07 23:20:05 +0530

    d

---
+++ wp-content/plugins/cm-widgets-group/cm-header-group.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Cm Widgets
+Description: A custom header widget to replace the Elementor header widget.
+Version: 1.0
+Author: Qualitapps
+Author URI: https://qualitapps.com
+*/
+
+// Register the custom widget with Elementor
+add_action('elementor/widgets/widgets_registered', function ($widgets_manager) {
+    // Include the widget class file
+    require_once(__DIR__ . '/widgets/cm-header-widget-group.php');
+
+    // Register the widget class
+    $widgets_manager->register(new \Elementor\Custom_Header_Group_Widget());
+});
